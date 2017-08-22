b81810c01e7e923548d1d10d56586bfc469acb1f
Oskar Weinfurtner <woskar@users.noreply.github.com>
2017-08-22 09:25:55 +0200

    Add files via upload

---
+++ F09-Neuromorphes-Rechnen/env.sh
@@ -0,0 +1,6 @@
+export PYTHONPATH=$PYTHONPATH:/home/default-fp-user/fp-spikey/lib:/home/default-fp-user/fp-spikey/lib/python2.7/site-packages
+export PYNN_HW_PATH=/home/default-fp-user/fp-spikey/deb-pynn/src/hardware/spikey
+export SPIKEYHALPATH=/home/default-fp-user/fp-spikey/spikeyhal
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/default-fp-user/fp-spikey/lib
+export PATH=$PATH:/home/default-fp-user/fp-spikey/bin
+
