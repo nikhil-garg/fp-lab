0a9625af1173fe39cacf1048797ccc7678535054
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-08-27 18:29:59 +0200

    script updated

--- E01-Elektronik-Grundpraktikum/Teil 1 und 2- Schaltungen/freq-plot.py
+++ E01-Elektronik-Grundpraktikum/Teil 1 und 2- Schaltungen/freq-plot.py
@@ -15,6 +15,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
+
+def plotten(x, y, xerror=None, yerror=None, 
+            xlim=None, ylim=None, 
+            xscale=None, yscale=None,
+            xlabel='x-axis', ylabel='y-axis', 
+            title=r'Plot-Title', 
+            save=False, name='plot.pdf', show=True):
+  plt.errorbar(x, y, marker='x', xerr= xerror, yerr=yerror, linestyle='--')
+  if xscale:
+    plt.xscale(xscale)
+  if yscale:
+    plt.yscale(yscale)
+  if xlim:
+    plt.xlim(xlim)
+  if ylim:
+    plt.ylim(ylim)
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.title(title)
+  if save:
+    plt.savefig(name)
+  if show:
+    plt.show()
+
 # Messwerte zur Emitterschaltung einlesen
 # Einlesen der Daten aus Tabelle 1
 f1, U_E1, dU_E1, U_A1, dU_A1 = np.loadtxt('tab1.txt', skiprows=1, usecols=(0,1,2,3,4), unpack=True)
@@ -23,38 +47,29 @@ f1, U_E1, dU_E1, U_A1, dU_A1 = np.loadtxt('tab1.txt', skiprows=1, usecols=(0,1,2
 V_U1 = U_A1 / U_E1
 dV_U1 = V_U1*np.sqrt((dU_A1/U_A1)**2+(dU_E1/U_E1)**2)
 
+
 # Plot 1 erstellen
 # Grenzfrequqenz der Emitterschaltung
 
-plt.yscale('log')
-plt.ylim((5,20))
-plt.xlim((-10, 200))
-plt.errorbar(f1, V_U1, marker='x', yerr=dV_U1, linestyle='--')
-plt.xlabel(r'Frequenz $f$ in Hz')
-plt.ylabel(r'Verstärkung $V_U$')
-plt.title(r'Plot 1: Grenzfrequenz Emitterschaltung')
-#plt.savefig('plot01.pdf',format='pdf')
-plt.show()
+plotten(f1, V_U1, yerror=dV_U1, xlim=(-10,200), ylim=(5,20), yscale='log', 
+  xlabel=r'Frequenz $f$ in Hz', ylabel=r'Verstärkung $V_U$', 
+  title=r'Plot 1: Grenzfrequenz Emitterschaltung',
+  name='plot01.pdf', save=False)
 
 
 # Plot 2 erstellen
 # Frequenzgang der Emitterschaltung
 
-plt.yscale('log')
-plt.xscale('log')
-plt.ylim((5,20))
-plt.xlim((10, 1800000))
-plt.errorbar(f1, V_U1, marker='x', yerr=dV_U1, linestyle='--')
-plt.xlabel('Frequenz $f$ in Hz')
-plt.ylabel('Verstärkung $V_U$')
-plt.title('Plot 2: Frequenzgang Emitterschaltung')
-#plt.savefig('plot02.pdf',format='pdf')
-plt.show()
+plotten(f1, V_U1, yerror=dV_U1, xlim=(10, 1800000), ylim=(5,20), 
+  xscale='log', yscale='log', 
+  xlabel=r'Frequenz $f$ in Hz', ylabel=r'Verstärkung $V_U$', 
+  title=r'Plot 2: Frequenzgang Emitterschaltung',
+  name='plot02.pdf', save=False)
 
-'''
+
 # Werte in Ausgabedatei schreiben:
 # Verstärkungen für Protokoll
-
+'''
 with open('daten1_achtung_neu_berechnet.txt', 'w') as file:
     file. write('f  U_E dU_E    U_A     dU_A    V_U dV_U\n')
     for i in range(len(f)):
@@ -68,6 +83,8 @@ with open('daten1_achtung_neu_berechnet.txt', 'w') as file:
 '''
 
 
+
+
 # Messwerte zur Kollektorschaltung einlesen
 # Einlesen der Daten aus Tabelle 2
 f2, U_E2, dU_E2, U_A2, dU_A2 = np.loadtxt('tab2.txt', skiprows=1, usecols=(0,1,2,3,4), unpack=True)
@@ -79,34 +96,25 @@ dV_U2 = V_U2*np.sqrt((dU_A2/U_A2)**2+(dU_E2/U_E2)**2)
 # Plot 3 erstellen
 # Grenzfrequqenz der Kollektorschaltung
 
-plt.yscale('log')
-plt.ylim((0.2,1.1))
-plt.xlim((-2, 230))
-plt.errorbar(f2, V_U2, marker='x', yerr=dV_U2, linestyle='--')
-plt.xlabel('Frequenz $f$ in Hz')
-plt.ylabel('Verstärkung $V_U$')
-plt.title('Plot 3: Grenzfrequenz Kollektorschaltung')
-#plt.savefig('plot03.pdf',format='pdf')
-plt.show()
+plotten(f2, V_U2, yerror=dV_U2, xlim=(-2, 230), ylim=(0.2,1.1), yscale='log', 
+  xlabel=r'Frequenz $f$ in Hz', ylabel=r'Verstärkung $V_U$', 
+  title=r'Plot 3: Grenzfrequenz Kollektorschaltung',
+  name='plot03.pdf', save=False)
 
 
 # Plot 4 erstellen
 # Frequenzgang der Kollektorschaltung
 
-plt.yscale('log')
-plt.xscale('log')
-plt.ylim((0.2,1.1))
-plt.xlim((8, 1000000))
-plt.errorbar(f2, V_U2, marker='x', yerr=dV_U2, linestyle='--')
-plt.xlabel('Frequenz $f$ in Hz')
-plt.ylabel('Verstärkung $V_U$')
-plt.title('Plot 4: Frequenzgang Kollektorschaltung')
-#plt.savefig('plot04.pdf',format='pdf')
-plt.show()
+plotten(f2, V_U2, yerror=dV_U2, xlim=(8, 1000000), ylim=(0.2,1.1), 
+  xscale='log', yscale='log', 
+  xlabel=r'Frequenz $f$ in Hz', ylabel=r'Verstärkung $V_U$', 
+  title=r'Plot 4: Frequenzgang Kollektorschaltung',
+  name='plot04.pdf', save=False)
 
+
+# Werte in Ausgabedatei schreiben:
 
 '''
-# Werte in Ausgabedatei schreiben:
 with open('daten2_achtung_neu_berechnet.txt', 'w') as file:
     file. write('f  U_E dU_E    U_A     dU_A    V_U dV_U\n')
     for i in range(len(f)):
@@ -117,6 +125,5 @@ with open('daten2_achtung_neu_berechnet.txt', 'w') as file:
                    str(dU_A2[i])+'   '+
                    str(round(V_U2[i],2))+'   '+
                    str(round(dV_U2[i],2))+'\n')
-
 '''                   
 
