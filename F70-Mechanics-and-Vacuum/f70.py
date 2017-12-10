21d4566ac1e97428d0590450b19333d5e04c3f9b
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-12-10 20:10:18 +0100

    new script

--- F70-Mechanics-and-Vacuum/f70.py
+++ F70-Mechanics-and-Vacuum/f70.py
@@ -4,3 +4,15 @@ Mechanics and Vacuum
 Fortgeschrittenen Praktikum Uni Heidelberg
 F70: Mechanik und Vakuuum 
 '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+tabellen = ['tab2.txt', 'tab3.txt', 'tab4.txt']
+
+for tab in tabellen:
+    c1, c2 = np.loadtxt(tab, unpack=True, skiprows=1)
+    plt.plot(c1, c2)
+    plt.title(tab)
+    plt.savefig(tab[:-3] + '.pdf')
+    plt.show()
