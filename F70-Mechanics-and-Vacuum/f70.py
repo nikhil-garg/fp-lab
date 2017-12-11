bc9983e2533506f1980c8cd6d618e708d087f13f
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-12-11 11:52:54 +0100

    script now plots also tab1

--- F70-Mechanics-and-Vacuum/f70.py
+++ F70-Mechanics-and-Vacuum/f70.py
@@ -8,8 +8,14 @@ F70: Mechanik und Vakuuum
 import numpy as np
 import matplotlib.pyplot as plt
 
-tabellen = ['tab2.txt', 'tab3.txt', 'tab4.txt']
 
+p, dp, t, dt, V, dV = np.loadtxt('tab1.txt', unpack=True, skiprows=1)
+plt.plot(p, V/t)
+plt.title(tab1.txt)
+plt.savefig('tab1.pdf')
+plt.show()
+
+tabellen = ['tab2.txt', 'tab3.txt', 'tab4.txt']
 for tab in tabellen:
     c1, c2 = np.loadtxt(tab, unpack=True, skiprows=1)
     plt.plot(c1, c2)
