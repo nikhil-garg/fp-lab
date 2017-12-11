d1bb5e402c9b2a1a190468b346e74b03fe8f0a01
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-12-11 12:07:34 +0100

    plots now loglog

--- F70-Mechanics-and-Vacuum/f70.py
+++ F70-Mechanics-and-Vacuum/f70.py
@@ -12,9 +12,11 @@ import matplotlib.pyplot as plt
 p, dp, t, dt, V, dV = np.loadtxt('tab1.txt', unpack=True, skiprows=1)
 plt.plot(p, V/t)
 plt.title('tab1.txt')
+plt.xscale('log')
+plt.yscale('log')
 plt.xlabel('p')
 plt.ylabel('V/t')
-plt.savefig('tab1.pdf')
+plt.savefig('tab1.png')
 plt.show()
 
 tabellen = ['tab2.txt', 'tab3.txt', 'tab4.txt']
@@ -22,7 +24,9 @@ for tab in tabellen:
     c1, c2 = np.loadtxt(tab, unpack=True, skiprows=1)
     plt.plot(c1, c2)
     plt.xlabel('c1')
+    plt.xscale('log')
+    plt.yscale('log')
     plt.ylabel('c2')
     plt.title(tab)
-    plt.savefig(tab[:-4] + '.pdf')
+    plt.savefig(tab[:-4] + '.png')
     plt.show()
