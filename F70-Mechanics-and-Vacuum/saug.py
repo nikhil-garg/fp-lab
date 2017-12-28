d81df2810a1acc79b6ecf96661acd555cbf95b86
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-12-28 19:12:57 +0100

    Saugleistung TMP Plots

--- F70-Mechanics-and-Vacuum/saug.py
+++ F70-Mechanics-and-Vacuum/saug.py
@@ -3,7 +3,7 @@ Mechanics and Vacuum
 
 Fortgeschrittenen Praktikum Uni Heidelberg
 F70: Mechanik und Vakuum 
-Teil 1: Auswertung Tabelle 1, Saugvermögen der TMP
+Teil 2.4: Auswertung Tabelle 1, Saugvermögen der TMP
 
 Plot: Saugvermögen als Funktion des Logarithmus des Drucks auftragen
 '''
@@ -20,12 +20,45 @@ def dS(V, dV, t, dt):
 
 
 p, dp, t, dt, V, dV = np.loadtxt('tab1.txt', unpack=True, skiprows=1)
-plt.errorbar(p, S(V, t), xerr=dp, yerr=dS(V, dV, t, dt), markersize='10', fmt='x', linestyle='-')
-plt.title('Plot1: Saugvermögen als Funktion des Logarithmus des Drucks')
+
+V = V * 0.001 #von milli Liter nach Liter umrechnen
+dV = dV * 0.001
+p = p *0.001
+dp = dp *0.001
+
+plt.errorbar(p[:6], S(V, t)[:6], xerr=dp[:6], yerr=dS(V, dV, t, dt)[:6], markersize='10', mew=1.2, fmt='x', label='Kapillare')
+plt.errorbar(p[6:], S(V, t)[6:], xerr=dp[6:], yerr=dS(V, dV, t, dt)[6:], markersize='10', fmt='x', mew=1.2, label='Kolbenprober')
+plt.title('Plot 1a: Saugvermögen S der Turbomolekularpumpe (TMP)')
+plt.legend(title='Messwerte', borderpad=1, borderaxespad=1, loc='best', shadow='true', fontsize=12)
 plt.xscale('log')
-plt.xlim(7e-6, 2)
-#plt.yscale('log')
-plt.xlabel('Druck (logarithmisch)')
+plt.yscale('log')
+plt.xlim(7e-9, 2e-3)
+plt.xlabel(r'Druck $p\ [bar]$')
 plt.ylabel(r'Saugvermögen $S = \frac{dV}{dt}$')
-plt.savefig('plot1.pdf')
+plt.savefig('plot1a.pdf')
 plt.show()
+
+plt.errorbar(p[:6], S(V, t)[:6]/p[:6], xerr=dp[:6], yerr=np.sqrt((dS(V, dV, t, dt)[:6]/p[:6])**2+(dp[:6]*S(V, t)[:6]/(p[:6])**2)**2), markersize='10', mew=1.2, fmt='x', label='Kapillare')
+plt.errorbar(p[6:], S(V, t)[6:]/p[6:], xerr=dp[6:], yerr=np.sqrt((dS(V, dV, t, dt)[6:]/p[6:])**2+(dp[6:]*S(V, t)[6:]/(p[6:])**2)**2), markersize='10', fmt='x', mew=1.2, label='Kolbenprober')
+plt.title('Plot 1b: Druckangepasstes Saugvermögen S/p der TMP')
+plt.legend(title='Messwerte', borderpad=1, borderaxespad=1, loc='best', shadow='true', fontsize=12)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(7e-9, 2e-3)
+plt.xlabel(r'Druck $p\ [bar]$')
+plt.ylabel(r'Druckangepasstes Saugvermögen $S_p = \frac{S}{p_a}$')
+plt.savefig('plot1b.pdf')
+plt.show()
+
+plt.errorbar(p[:6], S(V, t)[:6]*p[:6], xerr=dp[:6], yerr=np.sqrt((dS(V, dV, t, dt)[:6]*p[:6])**2+(dp[:6]*S(V, t)[:6])**2), markersize='10', mew=1.2, fmt='x', label='Kapillare')
+plt.errorbar(p[6:], S(V, t)[6:]*p[6:], xerr=dp[6:], yerr=np.sqrt((dS(V, dV, t, dt)[6:]*p[6:])**2+(dp[6:]*S(V, t)[6:])**2), markersize='10', fmt='x', mew=1.2, label='Kolbenprober')
+plt.title('Plot 1c: Saugleistung Q der Turbomolekularpumpe (TMP)')
+plt.legend(title='Messwerte', borderpad=1, borderaxespad=1, loc='best', shadow='true', fontsize=12)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(7e-9, 2e-3)
+plt.xlabel(r'Druck $p\ [bar]$')
+plt.ylabel(r'Saugleistung $Q = S \cdot p_a$')
+plt.savefig('plot1c.pdf')
+plt.show()
+
