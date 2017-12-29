31c1b4494cceb80aff11558d8382c4e975b140e4
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-12-29 11:24:40 +0100

    More plots for Saugleistung

--- F70-Mechanics-and-Vacuum/saug.py
+++ F70-Mechanics-and-Vacuum/saug.py
@@ -26,6 +26,11 @@ dV = dV * 0.001
 p = p *0.001
 dp = dp *0.001
 
+print('p:', p)
+print('dp:', dp)
+print('S:', S(V, t))
+print('dS:', dS(V, dV, t, dt))
+
 plt.errorbar(p[:6], S(V, t)[:6], xerr=dp[:6], yerr=dS(V, dV, t, dt)[:6], markersize='10', mew=1.2, fmt='x', label='Kapillare')
 plt.errorbar(p[6:], S(V, t)[6:], xerr=dp[6:], yerr=dS(V, dV, t, dt)[6:], markersize='10', fmt='x', mew=1.2, label='Kolbenprober')
 plt.title('Plot 1a: Saugvermögen S der Turbomolekularpumpe (TMP)')
@@ -35,7 +40,7 @@ plt.yscale('log')
 plt.xlim(7e-9, 2e-3)
 plt.xlabel(r'Druck $p\ [bar]$')
 plt.ylabel(r'Saugvermögen $S = \frac{dV}{dt}$')
-plt.savefig('plot1a.pdf')
+#plt.savefig('plot1a.pdf')
 plt.show()
 
 plt.errorbar(p[:6], S(V, t)[:6]/p[:6], xerr=dp[:6], yerr=np.sqrt((dS(V, dV, t, dt)[:6]/p[:6])**2+(dp[:6]*S(V, t)[:6]/(p[:6])**2)**2), markersize='10', mew=1.2, fmt='x', label='Kapillare')
@@ -47,7 +52,7 @@ plt.yscale('log')
 plt.xlim(7e-9, 2e-3)
 plt.xlabel(r'Druck $p\ [bar]$')
 plt.ylabel(r'Druckangepasstes Saugvermögen $S_p = \frac{S}{p_a}$')
-plt.savefig('plot1b.pdf')
+#plt.savefig('plot1b.pdf')
 plt.show()
 
 plt.errorbar(p[:6], S(V, t)[:6]*p[:6], xerr=dp[:6], yerr=np.sqrt((dS(V, dV, t, dt)[:6]*p[:6])**2+(dp[:6]*S(V, t)[:6])**2), markersize='10', mew=1.2, fmt='x', label='Kapillare')
@@ -59,6 +64,8 @@ plt.yscale('log')
 plt.xlim(7e-9, 2e-3)
 plt.xlabel(r'Druck $p\ [bar]$')
 plt.ylabel(r'Saugleistung $Q = S \cdot p_a$')
-plt.savefig('plot1c.pdf')
+#plt.savefig('plot1c.pdf')
 plt.show()
+
+
 
