fd4354e01f4e27f5451df4a2fad01af0eb39b92f
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2018-01-28 21:54:15 +0100

    review and comments

--- F70-Mechanics-and-Vacuum/Auswertung/leistung.py
+++ F70-Mechanics-and-Vacuum/Auswertung/leistung.py
@@ -7,7 +7,6 @@ F70: Mechanik und Vakuum
 Plot: Saugleistung der TMP = druckangepasstes Saugvermögen * Druck
 '''
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,8 +16,7 @@ def S(V, t):
 def dS(V, dV, t, dt):
     return np.sqrt(((1/t)*dV)**2 + ((V/t**2)*dt)**2)
 
-
-
+# Einlesen der ersten Messwerte
 p, dp, t, dt, V, dV = np.loadtxt('tab1.txt', unpack=True, skiprows=1)
 
 V = V * 0.001 #von MilliLiter nach Liter umrechnen
@@ -30,6 +28,7 @@ p_atmos = 1.01325 #mbar
 S_p = np.array(list(S(V, t)[:6]*(p_atmos/p[:6])) + list(S(V, t)[6:]*(p_atmos/p[6:])))
 dS_p = np.array(list(np.sqrt((dS(V, dV, t, dt)[:6]/p[:6])**2+(dp[:6]*S(V, t)[:6]/(p[:6])**2)**2)) + list(np.sqrt((dS(V, dV, t, dt)[6:]/p[6:])**2+(dp[6:]*S(V, t)[6:]/(p[6:])**2)**2)))
 
+# Plot erstellen
 plt.errorbar(p[:6], (S_p*p)[:6], xerr=dp[:6], yerr=(dS_p*p)[:6], markersize='10', mew=1.2, fmt='x', label='Kapillare')
 plt.errorbar(p[6:], (S_p*p)[6:], xerr=dp[6:], yerr=(dS_p*p)[6:], markersize='10', fmt='x', mew=1.2, label='Kolbenprober')
 
@@ -42,6 +41,4 @@ plt.xlabel(r'Druck $p\ [bar]$')
 plt.ylabel(r'Saugleistung $Q = S \cdot p_a \ [\frac{l}{s} \cdot bar] $')
 plt.savefig('Diagramm06.pdf')
 plt.show()
-
-
 
